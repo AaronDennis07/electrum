96d45159726fdfc26e840e0133bb4c0b8c3f1c81
Surya K N <95220715+Surya-KN@users.noreply.github.com>
2024-05-12 17:37:47 +0530

    add create sessions

--- internals/models/models.go
+++ internals/models/models.go
@@ -8,45 +8,46 @@ import (
 
 type Department struct {
 	gorm.Model
-	Name     *string `gorm:"unique"`
-	Admin    *string
-	Password *string
+	Name     *string `gorm:"unique" json:"name"`
+	Admin    *string `json:"admin"`
+	Password *string `json:"password"`
 }
 
 type Session struct {
 	gorm.Model
-	Name        *string
-	SessionType *string
+	Name        *string `json:"name"`
+	SessionType *string `json:"session_type"`
+	Courses     []Course
 }
 type Course struct {
 	gorm.Model
-	Name         *string
-	Code         *string
-	DepartmentID *uint
-	Department   Department
-	SessionID    *uint
-	Session      Session
+	Name         *string    `json:"name"`
+	Code         *string    `json:"code"`
+	DepartmentID *uint      `json:"department_id"`
+	Department   Department `json:"-"`
+	SessionID    *uint      `json:"session_id"`
+	Session      Session    `json:"-"`
 }
 
 type Student struct {
-	Usn          string `gorm:"primaryKey"`
-	Name         *string
-	Email        *string `gorm:"unique"`
-	Password     *string
-	DepartmentID *uint
-	Department   Department
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	DeletedAt    gorm.DeletedAt
+	Usn          string         `gorm:"primaryKey" json:"usn"`
+	Name         *string        `json:"name"`
+	Email        *string        `gorm:"unique" json:"email"`
+	Password     *string        `json:"-"`
+	DepartmentID *uint          `json:"department_id"`
+	Department   Department     `json:"-"`
+	CreatedAt    time.Time      `json:"-"`
+	UpdatedAt    time.Time      `json:"-"`
+	DeletedAt    gorm.DeletedAt `json:"-"`
 }
 type Enrollment struct {
 	gorm.Model
-	Course1ID *uint
-	Course1   Course
-	Course2ID *uint
-	Course2   Course
-	StudentID *string
-	Student   Student
-	SessionID *uint
-	Session   Session
+	Course1ID *uint   `json:"course1_id"`
+	Course1   Course  `json:"-"`
+	Course2ID *uint   `json:"course2_id"`
+	Course2   Course  `json:"-"`
+	StudentID *string `json:"student_id"`
+	Student   Student `json:"-"`
+	SessionID *uint   `json:"session_id"`
+	Session   Session `json:"-"`
 }
